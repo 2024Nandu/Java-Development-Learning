48dcd569dc515d74b2b622026efcf0c1257088b3
2024Nandu <nandkumarshinde61@gmail.com>
2026-03-04 23:26:12 +0530

    Learning java

---
+++ src/Govind.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Govind {
+
+	public static void main(String[] args) {
+		System.out.println("please enter your name: ");
+		Scanner input = new Scanner(System.in);
+		String name = input.nextLine();
+		System.out.println("Welcome " + name + " to KG Coding");
+	}
+}
